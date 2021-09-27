7382c47a3f8198628322b172a7d274777f4f4c6a
zengguoyang <qbjooo@qq.com>
2021-09-27 10:32:04 +0800

    init

---
+++ BMPretrain/layers/position_bias.py
@@ -0,0 +1,75 @@
+import torch
+
+class PositionBiasEmbedding(torch.nn.Module):
+    """Position bias embedding.
+
+    This is used to add bias to the positional embedding.
+    """
+
+    def __init__(self, num_buckets, num_heads):
+        super().__init__()
+        self.num_buckets = num_buckets
+        self.num_heads = num_heads
+        self.weight = torch.nn.Embedding(self.num_buckets, self.num_heads)
+
+    def _relative_position_bucket(self, relative_position, bidirectional=True, max_distance=128):
+        """
+        Adapted from Mesh Tensorflow:
+        https://github.com/tensorflow/mesh/blob/0cb87fe07da627bf0b7e60475d59f95ed6b5be3d/mesh_tensorflow/transformer/transformer_layers.py#L593
+
+        Translate relative position to a bucket number for relative attention. The relative position is defined as
+        memory_position - query_position, i.e. the distance in tokens from the attending position to the attended-to
+        position. If bidirectional=False, then positive relative positions are invalid. We use smaller buckets for
+        small absolute relative_position and larger buckets for larger absolute relative_positions. All relative
+        positions >=max_distance map to the same bucket. All relative positions <=-max_distance map to the same bucket.
+        This should allow for more graceful generalization to longer sequences than the model has been trained on
+
+        Args:
+            relative_position: an int32 Tensor
+            bidirectional: a boolean - whether the attention is bidirectional
+            num_buckets: an integer
+            max_distance: an integer
+
+        Returns:
+            a Tensor with the same shape as relative_position, containing int32 values in the range [0, num_buckets)
+        """
+        relative_buckets = 0
+        if bidirectional:
+            self.num_buckets //= 2
+            relative_buckets += (relative_position > 0).to(torch.long) * self.num_buckets
+            relative_position = torch.abs(relative_position)
+        else:
+            relative_position = -torch.min(relative_position, torch.zeros_like(relative_position))
+        # now relative_position is in the range [0, inf)
+
+        # half of the buckets are for exact increments in positions
+        max_exact = self.num_buckets // 2
+        is_small = relative_position < max_exact
+
+        # The other half of the buckets are for logarithmically bigger bins in positions up to max_distance
+        relative_postion_if_large = max_exact + (
+            torch.log(relative_position.float() / max_exact)
+            / math.log(max_distance / max_exact)
+            * (self.num_buckets - max_exact)
+        ).to(torch.long)
+        relative_postion_if_large = torch.min(
+            relative_postion_if_large, torch.full_like(relative_postion_if_large, self.num_buckets - 1)
+        )
+
+        relative_buckets += torch.where(is_small, relative_position, relative_postion_if_large)
+        return relative_buckets
+
+    def compute_bias(self, query_length, key_length):
+        """ Compute binned relative position bias """
+        context_position = torch.arange(query_length, dtype=torch.long)[:, None]
+        memory_position = torch.arange(key_length, dtype=torch.long)[None, :]
+        relative_position = memory_position - context_position  # shape (query_length, key_length)
+        relative_position_bucket = self._relative_position_bucket(
+            relative_position,  # shape (query_length, key_length)
+            bidirectional=(not self.is_decoder),
+            num_buckets=self.relative_attention_num_buckets,
+        )
+        relative_position_bucket = relative_position_bucket.to(self.relative_attention_bias.weight.device)
+        values = self.relative_attention_bias(relative_position_bucket)  # shape (query_length, key_length, num_heads)
+        values = values.permute([2, 0, 1]).unsqueeze(0)  # shape (1, num_heads, query_length, key_length)
+        return values
---
+++ csrc/igemm/igemm_kernel.cu
@@ -0,0 +1,157 @@
+#include "healper.h"
+#include <torch/extension.h>
+
+namespace {
+
+template<typename scalar_t, bool broad_cast1, bool broad_cast2>
+__global__ void scale_2dim_kernel(
+    const torch::PackedTensorAccessor32<int32_t, 3, torch::RestrictPtrTraits> input_tensor,
+    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> scale_1,
+    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> scale_2,
+    torch::PackedTensorAccessor32<scalar_t, 3, torch::RestrictPtrTraits> out
+    ) {
+    
+    const int n = blockIdx.z;
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x < out.size(1) && y < out.size(2)) {
+        if (!broad_cast1 && !broad_cast2) {
+            out[n][x][y] = (scalar_t)((float)(input_tensor[n][x][y]) * (float)(scale_1[n][x]) * (float)(scale_2[n][y]));
+        }
+        else if(broad_cast1 && !broad_cast2) {
+            out[n][x][y] = (scalar_t)((float)(input_tensor[n][x][y]) * (float)(scale_1[0][x]) * (float)(scale_2[n][y]));
+        }
+        else if(!broad_cast1 && broad_cast2) {
+            out[n][x][y] = (scalar_t)((float)(input_tensor[n][x][y]) * (float)(scale_1[n][x]) * (float)(scale_2[0][y]));
+        }
+        else if (broad_cast1 && broad_cast2) {
+            out[n][x][y] = (scalar_t)((float)(input_tensor[n][x][y]) * (float)(scale_1[0][x]) * (float)(scale_2[0][y]));
+        }
+    }
+}
+
+}
+
+void* create_cublaslt_handle() {
+    cublasLtHandle_t handle;
+    checkCublasStatus(cublasLtCreate(&handle));
+    return (void*)handle;
+}
+
+
+void scale_2dim(
+    torch::Tensor x,
+    torch::Tensor scale_1,
+    torch::Tensor scale_2,
+    torch::Tensor out
+) {
+    int32_t thread_size = 32;
+    const dim3 threads(thread_size, thread_size);
+    const dim3 blocks((x.size(1) + thread_size - 1) / thread_size, (x.size(2) + thread_size - 1) / thread_size, x.size(0));
+
+    AT_DISPATCH_FLOATING_TYPES_AND_HALF(out.type(), "i8linear_scale_cuda", ([&] {
+        if (scale_1.size(0) == 1 && scale_2.size(0) == 1) {
+            scale_2dim_kernel<scalar_t, true, true><<<blocks,threads>>>(
+                x.packed_accessor32<int32_t, 3, torch::RestrictPtrTraits>(),
+                scale_1.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                scale_2.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                out.packed_accessor32<scalar_t, 3, torch::RestrictPtrTraits>()
+            );
+        }
+        else if (scale_1.size(0) != 1 && scale_2.size(0) == 1) {
+            scale_2dim_kernel<scalar_t, false, true><<<blocks,threads>>>(
+                x.packed_accessor32<int32_t, 3, torch::RestrictPtrTraits>(),
+                scale_1.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                scale_2.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                out.packed_accessor32<scalar_t, 3, torch::RestrictPtrTraits>()
+            );
+        }
+        else if (scale_1.size(0) == 1 && scale_2.size(0) != 1) {
+            scale_2dim_kernel<scalar_t, true, false><<<blocks,threads>>>(
+                x.packed_accessor32<int32_t, 3, torch::RestrictPtrTraits>(),
+                scale_1.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                scale_2.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                out.packed_accessor32<scalar_t, 3, torch::RestrictPtrTraits>()
+            );
+        }
+        else if (scale_1.size(0) != 1 && scale_2.size(0) != 1) {
+            scale_2dim_kernel<scalar_t, false, false><<<blocks,threads>>>(
+                x.packed_accessor32<int32_t, 3, torch::RestrictPtrTraits>(),
+                scale_1.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                scale_2.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+                out.packed_accessor32<scalar_t, 3, torch::RestrictPtrTraits>()
+            );
+        }
+    }));
+    
+}
+
+void LtIgemm(cublasLtHandle_t ltHandle,
+                   int m,
+                   int n,
+                   int k,
+                   const int8_t *A,
+                   int64_t stride_a,
+                   const int8_t *B,
+                   int64_t stride_b,
+                   int32_t *C,
+                   int64_t stride_c,
+                   int32_t num_batches) {
+    cublasLtMatmulDesc_t matmulDesc = NULL;
+    cublasLtMatrixLayout_t Adesc = NULL, Bdesc = NULL, Cdesc = NULL;
+    int32_t alpha = 1, beta = 0;
+    cublasOperation_t opTranspose = CUBLAS_OP_T;
+
+    // ---------------------------------------------------------------------------------------------
+    // init MatMul Desc
+
+#if (CUDART_VERSION >= 11000)
+    checkCublasStatus(cublasLtMatmulDescCreate(&matmulDesc, CUBLAS_COMPUTE_32I, CUDA_R_32I));
+#else
+    checkCublasStatus(cublasLtMatmulDescCreate(&matmulDesc, CUDA_R_32I));
+#endif
+
+    
+    // transA
+    checkCublasStatus(cublasLtMatmulDescSetAttribute(matmulDesc, CUBLASLT_MATMUL_DESC_TRANSA, &opTranspose, sizeof(opTranspose)));
+    
+    // ---------------------------------------------------------------------------------------------
+    // create descriptors for original matrices
+    checkCublasStatus(cublasLtMatrixLayoutCreate(&Adesc, CUDA_R_8I, k, m, k));
+    checkCublasStatus(cublasLtMatrixLayoutCreate(&Bdesc, CUDA_R_8I, k, n, k));
+    checkCublasStatus(cublasLtMatrixLayoutCreate(&Cdesc, CUDA_R_32I, m, n, m));
+
+    checkCublasStatus(cublasLtMatrixLayoutSetAttribute(Adesc, CUBLASLT_MATRIX_LAYOUT_BATCH_COUNT, &num_batches, sizeof(num_batches)));
+    checkCublasStatus(cublasLtMatrixLayoutSetAttribute(Bdesc, CUBLASLT_MATRIX_LAYOUT_BATCH_COUNT, &num_batches, sizeof(num_batches)));
+    checkCublasStatus(cublasLtMatrixLayoutSetAttribute(Cdesc, CUBLASLT_MATRIX_LAYOUT_BATCH_COUNT, &num_batches, sizeof(num_batches)));
+
+    checkCublasStatus(cublasLtMatrixLayoutSetAttribute(Adesc, CUBLASLT_MATRIX_LAYOUT_STRIDED_BATCH_OFFSET, &stride_a, sizeof(stride_a)));
+    checkCublasStatus(cublasLtMatrixLayoutSetAttribute(Bdesc, CUBLASLT_MATRIX_LAYOUT_STRIDED_BATCH_OFFSET, &stride_b, sizeof(stride_b)));
+    checkCublasStatus(cublasLtMatrixLayoutSetAttribute(Cdesc, CUBLASLT_MATRIX_LAYOUT_STRIDED_BATCH_OFFSET, &stride_c, sizeof(stride_c)));
+
+    // ---------------------------------------------------------------------------------------------
+    // transforms and computation
+    // no need to transform C matrix as beta is assumed to be 0
+    checkCublasStatus(cublasLtMatmul(ltHandle,
+                                     matmulDesc,
+                                     &alpha,
+                                     A,
+                                     Adesc,
+                                     B,
+                                     Bdesc,
+                                     &beta,
+                                     C,
+                                     Cdesc,
+                                     C,
+                                     Cdesc,
+                                     NULL,
+                                     NULL,
+                                     0,
+                                     0));
+
+    if (Cdesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Cdesc));
+    if (Bdesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Bdesc));
+    if (Adesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Adesc));
+    if (matmulDesc) checkCublasStatus(cublasLtMatmulDescDestroy(matmulDesc));
+}
