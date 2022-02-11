9570cd2dc34d26cb6cca015c4e22f0bc8e21b0c4
zgy <qbjooo@qq.com>
2022-02-11 06:51:45 +0000

    remove run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-# torchrun --nnodes=1 --nproc_per_node=4 --rdzv_id=1 --rdzv_backend=c10d --rdzv_endpoint=localhost test.py
-python -m torch.distributed.run --nnodes=1 --nproc_per_node=4 --rdzv_id=1 --rdzv_backend=c10d --rdzv_endpoint=localhost test.py
