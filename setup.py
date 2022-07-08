4fa4108225a2b2d0c5be4af2ccd44ba0f69746ab
zgy <qbjooo@qq.com>
2022-07-08 16:41:18 +0800

    update version

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ else:
 
 setup(
     name='bmtrain',
-    version='0.1.7.post1',
+    version='0.1.8',
     author="Guoyang Zeng",
     author_email="qbjooo@qq.com",
     description="A toolkit for training big models",
