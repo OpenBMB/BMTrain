e15e3660b23acc4644bf40b2f701eeac62bc6d49
yangxiangjun <yangxiangjun@zhihu.com>
2023-04-26 14:02:11 +0800

    update statsd

--- example/statsd.py
+++ example/statsd.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import os
 import time
 from pstatsd import Statsd
 
@@ -20,13 +20,25 @@ STATSD = None
 
 class Statsds:
 
-    def __init__(self, log_name = None):
-        self.log_name = log_name
+    def __init__(self):
+
+        node_name = os.getenv("NODE_NAME", "jeeves-hpc-gpu00").replace(".", "_")
+        rank_name = (int)(os.getenv("RANK", "0")) // 8
+        project_name = os.getenv("PROJECT_NAME", "no-project-name")
+        job_id = os.getenv("JEEVES_JOB_ID", "000")
+        status_prefix = "job-status.{}.{}.{}.{}.step".format(
+            project_name,
+            job_id,
+            rank_name,
+            node_name,
+        )
+
+        self.status_prefix = status_prefix
         Statsd.initialize(app_name="luca-model", host="status.pek01.rack.zhihu.com")
         self.statsd = Statsd
 
     def gauges(self, name, value, repeat=1):
-        name = self.log_name + '.' + name
+        name = self.status_prefix + '.' + name
         try:
             self.statsd.gauge(name, value)
             for i in range(repeat - 1):
@@ -37,7 +49,7 @@ class Statsds:
         return None
 
     def incr(self, name, count=1):
-        name = self.log_name + '.' + name
+        name = self.status_prefix + '.' + name
         try:
             self.statsd.incr(name, count = count)
         except Exception as e:
@@ -45,16 +57,17 @@ class Statsds:
         return None
 
     def timing(self, name, value):
-        name = self.log_name + '.' + name
+        name = self.status_prefix + '.' + name
         try:
             self.statsd.timing(name, value)
         except Exception as e:
             print(u'statsd.timing failed, detail msg: {}'.format(e))
         return None
 
-def get_statsd(log_name = None):
+def get_statsd():
     global STATSD
     if STATSD is not None:
         return STATSD
-    STATSD = Statsds(log_name)
+
+    STATSD = Statsds()
     return STATSD
