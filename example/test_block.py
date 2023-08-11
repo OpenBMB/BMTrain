683707d02ae3beb91e4a5be9028ddba358eafebc
zhangkaihuo <zhangkaihuo@modelbest.cn>
2023-08-11 18:41:49 +0800

    test

--- example/test_block.py
+++ example/test_block.py
@@ -6,6 +6,15 @@ from layers import TransformerEncoder
 
 
 gb = 1024.0 * 1024.0 * 1024.0
+device = torch.device('cuda')
+
+def reserved(device):
+    return torch.cuda.memory_reserved(device) / gb
+def allocated(device):
+    return torch.cuda.memory_allocated(device) / gb
+def max_allocated(device):
+    return torch.cuda.max_memory_allocated(device) / gb
+
 
 bmt.init_distributed(zero_level=3)
 
@@ -24,25 +33,24 @@ for i in range(10), :
 
 linears = bmt.TransformerBlockList(linears)
 
-device = torch.device('cuda')
 bmt.synchronize()
 if config['rank'] == 0:
-	print('before forward', torch.cuda.memory_allocated(device) / gb)
+    print('before forward',  allocated(device), reserved(device), max_allocated(device))
 batch_size=1
 seq_len=4096
 x = torch.randn(batch_size, seq_len, 8192, dtype=torch.float16, device=device).requires_grad_()
 bmt.synchronize()
 if config['rank'] == 0:
-	print('init input', torch.cuda.memory_allocated(device) / gb)
+    print('init input',  allocated(device), reserved(device), max_allocated(device))
 enc_length = torch.randint(128, seq_len, (batch_size,)).long().cuda()
 mask = torch.arange(seq_len).unsqueeze(0) <= torch.arange(seq_len).unsqueeze(1)
-mask = mask.unsqueeze(0).unsqueeze(0)
+mask = mask.unsqueeze(0).unsqueeze(0).to(device)
 y = linears(x,mask)
 bmt.synchronize()
 if config['rank'] == 0:
-	print('after forward', torch.cuda.memory_allocated(device) / gb)
+    print('after forward',  allocated(device), reserved(device), max_allocated(device))
 
 y.sum().backward()
 bmt.synchronize()
 if config['rank'] == 0:
-	print('after backward', torch.cuda.memory_allocated(device) / gb)
+    print('after backward',  allocated(device), reserved(device), max_allocated(device))
