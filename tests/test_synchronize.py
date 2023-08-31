9bbd27999adc40fbeb94feb2be791135b923491a
Congrui Yin <88324880+JerryYin777@users.noreply.github.com>
2023-08-31 01:15:35 -0700

    FIX error of multi-gpus of test_synchronize.py

--- tests/test_synchronize.py
+++ tests/test_synchronize.py
@@ -7,17 +7,18 @@ from bmtrain.synchronize import gather_result
 
 def test_main():
 
-    ref_result = torch.rand(5 * bmt.world_size(), 5)  
+    ref_result = torch.ones(5 * bmt.world_size(), 5)  
     tensor = ref_result.chunk(bmt.world_size(), dim=0)[bmt.rank()]
     real_result = bmt.gather_result(tensor)
     assert torch.allclose(ref_result, real_result, atol=1e-6), "Assertion failed for real gather result error"
 
     for i in range(4):
-        size = i + 1
+        size = i + 1  
         tensor_slice = tensor[:size, :size]
         result_slice = bmt.gather_result(tensor_slice)
-        assert torch.allclose(real_result[:size, :size], result_slice, atol=1e-6), f"Assertion failed for tensor_slice_{i}"
-
+        test_slice = torch.chunk(result_slice, bmt.world_size(), dim=0)[0]
+        assert torch.allclose(tensor_slice, test_slice), f"Assertion failed for tensor_slice_{i}"
+ 
     print("All tensor slice tests passed!")
 
 if __name__ == '__main__':
