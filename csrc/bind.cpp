fd4fc05dbd49acb7908666d49dc2b29706463238
MayDomine <1583143678@qq.com>
2023-11-21 13:36:36 +0800

    delete cuda file and related binding

--- csrc/bind.cpp
+++ csrc/bind.cpp
@@ -1,12 +1,6 @@
 #include "include/bind.hpp"
 
 PYBIND11_MODULE(C, m) {
-    m.def("has_nan_inf_launcher",&has_nan_inf_launcher,"has nan inf");
-    m.def("adam_launcher", &adam_launcher, "adam function cpu");
-    m.def("cross_entropy_forward_launcher", &cross_entropy_forward_launcher, "cross entropy forward");
-    m.def("cross_entropy_backward_launcher", &cross_entropy_backward_launcher, "cross entropy backward");
-    m.def("cross_entropy_forward_inplace_launcher", &cross_entropy_forward_inplace_launcher, "cross entropy forward inplace");
-    m.def("cross_entropy_backward_inplace_launcher", &cross_entropy_backward_inplace_launcher, "cross entropy backward inplace");
     m.def("ncclGetUniqueId", &pyNCCLGetUniqueID, "nccl get unique ID");
     m.def("ncclCommInitRank", &pyNCCLCommInitRank, "nccl init rank");
     m.def("ncclCommDestroy", &pyNCCLCommDestroy, "nccl delete rank");
