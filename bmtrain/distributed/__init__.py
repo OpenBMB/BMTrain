d658b12ae3d9d1a59b4afeb5c1f51e3c949fd0fe
MayDomine <1583143678@qq.com>
2024-05-28 12:58:01 +0800

    benchmark and ops update

--- bmtrain/distributed/__init__.py
+++ bmtrain/distributed/__init__.py
@@ -1,1 +1,1 @@
-from .ops import all_gather, all_reduce, broadcast, recv_activations, send_activations, reduce_scatter
+from .ops import all_gather, all_reduce, broadcast, recv_activations, send_activations, reduce_scatter, all_to_all
